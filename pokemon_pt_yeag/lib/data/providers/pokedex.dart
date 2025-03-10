ca4f7938da66ae902b1eb22fb8a15d91a51f609f
tebazile <yadira.eag.elf@gmail.com>
2025-03-10 12:24:50 -0600

    feat: provider

---
+++ pokemon_pt_yeag/lib/data/providers/pokedex.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_pt_yeag/data/models/pokedex.dart';
+import 'package:pokemon_pt_yeag/data/models/pokemon.dart';
+import 'package:pokemon_pt_yeag/data/models/species.dart';
+import 'package:pokemon_pt_yeag/data/services/pokedex.dart';
+
+class PokedexProvider with ChangeNotifier {
+  bool _isLoading = false;
+  String _searchQuery = '';
+  List<Result> _pokedex = [];
+  final Map<String, Pokemon> _pokemonData = {};
+  final Map<String, PokemonSpecies> _pokemonSpeciesData = {};
+  bool _isSortedByName = false;
+  final PokedexService service;
+
+  PokedexProvider(this.service);
+
+  bool get isLoading => _isLoading;
+  bool get isSortedByName => _isSortedByName;
+  Map<String, Pokemon> get pokemonData => _pokemonData;
+  Map<String, PokemonSpecies> get pokemonSpeciesData => _pokemonSpeciesData;
+  // Get Pokemon List
+  List<Result> get pokedex {
+    List<Result> sortedPokedex = List.from(_pokedex);
+    if (_isSortedByName) {
+      sortedPokedex.sort((a, b) => a.name.compareTo(b.name));
+    } else {
+      sortedPokedex.sort((a, b) =>
+          _pokemonData[a.name]!.id.compareTo(_pokemonData[b.name]!.id));
+    }
+    return _searchQuery.isEmpty
+        ? sortedPokedex
+        : sortedPokedex
+            .where((result) => result.name.contains(_searchQuery.toLowerCase()))
+            .toList();
+  }
+
+  void setSearchQuery(String query) {
+    _searchQuery = query;
+    notifyListeners();
+  }
+
+  void toggleSortOrder() {
+    _isSortedByName = !_isSortedByName;
+    notifyListeners();
+  }
+
+  Future<void> fetchPokedex() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _pokedex = await service.getPokedexList();
+      await fetchAllPokemonData();
+    } catch (e) {
+      //Manage errors
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> fetchAllPokemonData() async {
+    for (var result in _pokedex) {
+      await fetchPokemonData(result.url);
+    }
+  }
+
+  Future<void> fetchPokemonData(String url) async {
+    try {
+      final pokemon = await service.getPokemonData(url);
+      _pokemonData[pokemon.name] = pokemon;
+
+      final speciesUrl = pokemon.species.url;
+      final speciesData = await service.getPokemonSpeciesData(speciesUrl);
+      _pokemonSpeciesData[pokemon.name] = speciesData;
+      notifyListeners();
+    } catch (e) {
+      //Manage errors
+    }
+  }
+}
